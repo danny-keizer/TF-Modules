df898a52e907f7091160de8ee517ee3a09dc802a
danny.keizer@student.hu.nl <danny.keizer@student.hu.nl>
2024-10-30 11:41:26 +0100

    Added skeleton module

---
+++ hcl-skeleton-module/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  description = "The name of the example resource"
+  type        = string
+}
+
+variable "resource_group" {
+  description = "The resource group the example resource will belong to"
+  type        = string
+}
